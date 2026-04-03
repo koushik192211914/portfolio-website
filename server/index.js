91ad5fbef9d122c8d97733fb4158e91b4ee54795
koushik192211914 <jyothikoushik2005@gmail.com>
2026-04-04 00:27:11 +0530

    enabled request handling with cors and json parsing

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Backend running');
